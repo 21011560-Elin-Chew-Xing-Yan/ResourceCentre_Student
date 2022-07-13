04faf0c96866011a07dc77ae4d2669018077dc7e
21024059 <21024059@LAPTOP-98E7TLMH>
2022-07-13 14:08:46 +0800

    Jing En First commit

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -206,6 +206,7 @@ public class ResourceCentreTest {
 		assertTrue("Test if loaned out camcorder CC0012 is returned- true", isReturned);
 		//error
 		isReturned = ResourceCentre.doReturnCamcorder(camcorderList, "CC0013");
+		
 		assertFalse("Test if non-existing camcorder CC0013 is returned - false?", isReturned);
 	}
 	
